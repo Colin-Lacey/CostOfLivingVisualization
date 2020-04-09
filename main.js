a992ff539c2a82c4f6424c251659c56c5e260f68
Nicolas Gonzalez <nicolas.g96@gmail.com>
2020-04-09 16:20:01 -0600

    working on layout

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ let costOfLivingData;
 
 // read in data
 Promise.all([
-  d3.json("//unpkg.com/world-atlas@1/world/110m.json") ,
+  d3.json("//unpkg.com/world-atlas@1/world/110m.json"),
   d3.csv("cost_of_living.csv"),
   d3.csv('city_coordinates.csv')
 ]).then(initialize);
