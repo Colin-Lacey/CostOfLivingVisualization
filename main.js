c8f8ae1c53f33cbcfca264b6856223696b9e1d17
Nicolas Gonzalez <nicolas.g96@gmail.com>
2020-04-09 21:33:03 -0600

    fix spaces and tabs

--- main.js
+++ main.js
@@ -5,34 +5,34 @@ let g_costOfLivingData;
 
 // read in data
 Promise.all([
-  d3.json("//unpkg.com/world-atlas@1/world/110m.json"),
-  d3.csv("cost_of_living.csv"),
-  d3.csv('city_coordinates.csv')
+	d3.json("//unpkg.com/world-atlas@1/world/110m.json"),
+	d3.csv("cost_of_living.csv"),
+	d3.csv('city_coordinates.csv')
 ]).then(initialize);
 
 function initialize(data){
-  g_costOfLivingData = data[1];
-  initializeMap(data[0], data[2]);
+	g_costOfLivingData = data[1];
+	initializeMap(data[0], data[2]);
 }
 
 function onMouseOverCity(cityData){
-  console.log("Moused over:");
-  console.log(cityData);
+	console.log("Moused over:");
+	console.log(cityData);
 }
 
 function onMouseOutOfCity(cityData){
-  console.log("Moused out of:");
-  console.log(cityData);
+	console.log("Moused out of:");
+	console.log(cityData);
 }
 
 function onCitySelected(cityData){
-  console.log("Selected:");
-  console.log(cityData);
+	console.log("Selected:");
+	console.log(cityData);
 }
 
 function onCityDeselected(cityData){
-  console.log("Deselected:");
-  console.log(cityData);
+	console.log("Deselected:");
+	console.log(cityData);
 }
 
 function getSelectedCities(){
