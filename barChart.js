989474bec808193df14e097bf2e8a90dcc164f6f
Colin-Lacey <colilace@gmail.com>
2020-04-11 13:58:01 -0600

    redid chart including axes and city name

--- barChart.js
+++ barChart.js
@@ -8,52 +8,77 @@ function onMouseOverCity_BarChart(event, cityData) {
 	console.log("Bar chart: moused over:");
 	console.log(cityData);
 
-	$("#barChart").append("<p>"+cityData.City+"</p>");
+	$("#cityName").html("<b>"+cityData.City+"</b>");
 
-	let browsers = [];
-	let w = 400;
-	let h = 300;
-	let barPadding = 0;
+	// following code modified from https://bl.ocks.org/alex-rind/0db75664782bd4ecdcbc93787ed07597
 
-	var svg = d3.select("#barContainer").append("svg")
-		.attr("width", w)
-		.attr("height", h)
-		.attr("id", "barChart");
+	var svg = d3.select("#barchart"),
+		margin = { top: 20, right: 20, bottom: 30, left: 40 },
+		width = +400 - margin.left - margin.right,
+		height = +280 - margin.top - margin.bottom;
 
-	svg.selectAll("rect")
-		.data([cityData["Cost of Living Index"], cityData["Rent Index"], cityData["Groceries Index"], cityData["Restaurant Price Index"], cityData["Local Purchasing Power Index"]])
-		.enter()
-		.append("rect")
-		.attr("x", function (d, i) {
-			return i * (w / 5);
-		})
-		.attr("y", function (d) {
-			return parseInt(h - d);
-		})
-		.attr("width", (w / 5) - barPadding)
-		.attr("height", function (d) {
-			return parseInt(d);
-		})
-		.attr("fill", function (d, i) {
-			var rgbColor = "rgb(" + Math.round((Math.random() * 255)) +
-				", " + Math.round((Math.random() * 255)) + ", " +
-				Math.round((Math.random() * 255)) + ")";
-			return rgbColor;
+	//width = +svg.attr("width") - margin.left - margin.right,
+	//height = +svg.attr("height") - margin.top - margin.bottom;
+
+	var x = d3.scaleBand().rangeRound([0, width]).padding(0.1),
+		y = d3.scaleLinear().rangeRound([height, 0]);
+
+	var g = svg.append("g")
+		.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+	const data = [
+		{ "Index": "COL", "Value": cityData["Cost of Living Index"] },
+		{ "Index": "Groceries", "Value": cityData["Groceries Index"] },
+		{ "Index": "Restaurants", "Value": cityData["Restaurant Price Index"] },
+		{ "Index": "Rent", "Value": cityData["Rent Index"] },
+		{ "Index": "Purchasing Power", "Value": cityData["Local Purchasing Power Index"] }
+	]
+
+
+	x.domain(data.map(function (d) { return d.Index; }));
+	y.domain([0, 140]);
+
+	g.append("g")
+		.attr("class", "axis axis--x")
+		.attr("transform", "translate(0," + height + ")")
+		.call(d3.axisBottom(x));
+
+	g.append("g")
+		.attr("class", "axis axis--y")
+		.call(d3.axisLeft(y))
+		.append("text")
+		.attr("transform", "rotate(-90)")
+		.attr("y", 6)
+		.attr("dy", "0.71em")
+		.attr("text-anchor", "end")
+		.text("Frequency");
+
+	g.selectAll(".bar")
+		.data(data)
+		.enter().append("rect")
+		.attr("class", "bar")
+		.attr("x", function (d) { return x(d.Index); })
+		.attr("y", function (d) { return y(parseInt(d.Value)); })
+		.attr("width", x.bandwidth())
+		.attr("height", function (d) { return height - y(parseInt(d.Value)); })
+		.attr("fill", function(d) {
+			switch(d.Index) {
+				case "COL": return "#400000";
+				case "Groceries": return"#7c0010";
+				case "Restaurants": return"#bd0011";
+				case "Rent": return "#ff1600";
+				case "Purchasing Power": return "#000080";
+			}
 		});
-
-	// Add a label to each SVG.
-	svg.append("text")
-	.attr("x", 0)
-    .attr("y", 10)
-    .attr("dy", ".35em")
-    .text(cityData.City);
 }
+
+
 
 
 function onMouseOutOfCity_BarChart(event, cityData) {
 	console.log("Bar chart: mouse out of:");
 	console.log(cityData);
 
-	d3.select("#barChart").remove();
+	d3.select("#barchart").selectAll('g').remove();
 
 }
