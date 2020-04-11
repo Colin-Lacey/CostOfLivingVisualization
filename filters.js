0cbffdb15deadda2a9268ae8ed37bd135b39771d
Nicolas Gonzalez <nicolas.g96@gmail.com>
2020-04-10 23:15:54 -0600

    improving aesthetics

--- filters.js
+++ filters.js
@@ -5,7 +5,7 @@ function initializeFilters(){
 		.sliderBottom()
 		.min(d3.min(data))
 		.max(d3.max(data))
-		.width(180)
+		.width(205)
 		.ticks(5)
 		.default([0, 130])
 		.fill('#2196f3')
@@ -20,7 +20,7 @@ function initializeFilters(){
 		.attr('width', 240)
 		.attr('height', 100)
 		.append('g')
-		.attr('transform', 'translate(30,30)');
+		.attr('transform', 'translate(20,25)');
 
 	gRange.call(sliderRange);
 
