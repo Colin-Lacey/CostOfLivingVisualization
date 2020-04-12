f11b9ce3ab5dcc7326668b7617a7bc430afa2425
RyanDoering <32683298+RyanDoering@users.noreply.github.com>
2020-04-12 14:39:22 -0600

    Update scatterPlot.js

--- scatterPlot.js
+++ scatterPlot.js
@@ -1,20 +1,60 @@
-
 function initializeScatterPlot(){
+
+	//Setting up the axis for each of the indicies at the top 
+	var vis = d3.selectAll('#axis');
+	var xScale = d3.scaleLinear()
+ 		.domain([0, 140])
+		.range([0, 115]);
+
+	var xAxis = d3.axisBottom(xScale)
+		.ticks(3)
+		.tickValues(d3.range(0, 141, 70));
+		 
+	vis.append('g')
+		.style("font", "8px Arial")
+		.call(xAxis);
+	//need to change the placement of the 0, getscut off a bit
+
+
 	$.subscribe("citySelected", onCitySelected_ScatterPlot);
 	$.subscribe("cityDeselected", onCityDeselected_ScatterPlot);
 	$.subscribe("citySelectionCleared", onCitySelectionCleared_ScatterPlot);
 }
 
-function onCitySelected_ScatterPlot(event, cityData){
+function onCitySelected_ScatterPlot(event, cityData) 
+{
 	console.log("Scatter plot: City selected:");
-	console.log(cityData);
+	console.log(cityData.City);
+	console.log(cityData['Cost of Living Index']);
+
+	var aDiv = document.createElement('div');
+	aDiv.id = cityData.City;7
+	document.getElementById('plot').appendChild(aDiv);
+
+	//var select = document.getElementById(cityData.City);
+	//select.innerHTML += cityData.City;
+
+	var indicies = ['City', 'Cost', 'Rent', 'Groceries', 'Restaurant', 'Local'];
+
+	for (var i = 0; i < indicies.length; i++)
+	{
+		var anSvg = document.createElement('svg');
+		anSvg.id = cityData.City+indicies[i];
+		document.getElementById(cityData.City).appendChild(anSvg);
+	}
+	var select = document.getElementById(cityData.City+"City");
+	select.innerHTML += cityData.City;
+
+	//d3 for adding the dots here
 }
 
-function onCityDeselected_ScatterPlot(event, cityData){
+function onCityDeselected_ScatterPlot(event, cityData) 
+{
 	console.log("Scatter plot: City deselected:");
 	console.log(cityData);
 }
 
-function onCitySelectionCleared_ScatterPlot(event){
+function onCitySelectionCleared_ScatterPlot(event) 
+{
 	console.log("Scatter plot: City selection cleared.");
 }
