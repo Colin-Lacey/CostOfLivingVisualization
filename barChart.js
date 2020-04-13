42fa68a7e3d3d5e91904b138203b266dfdbc2f08
Colin-Lacey <colilace@gmail.com>
2020-04-12 22:31:25 -0600

    redid colors and changed chart initialization/transitions

--- barChart.js
+++ barChart.js
@@ -2,6 +2,40 @@
 function initializeBarChart() {
 	$.subscribe("mouseOverCity", onMouseOverCity_BarChart);
 	$.subscribe("mouseOutOfCity", onMouseOutOfCity_BarChart);
+
+	// following code modified from https://bl.ocks.org/alex-rind/0db75664782bd4ecdcbc93787ed07597
+
+	let svg = d3.select("#barchart"),
+		margin = { top: 20, right: 20, bottom: 30, left: 40 },
+		width = +400 - margin.left - margin.right,
+		height = +280 - margin.top - margin.bottom;
+
+	//width = +svg.attr("width") - margin.left - margin.right,
+	//height = +svg.attr("height") - margin.top - margin.bottom;
+
+	var x = d3.scaleBand().rangeRound([0, width]).padding(0.1),
+		y = d3.scaleLinear().rangeRound([height, 0]);
+
+	var g = svg.append("g")
+		.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+		x.domain(["Cost Living","Groceries","Restaurants","Rent","Purchasing Power"]);
+		y.domain([0, 160]);
+	
+		g.append("g")
+			.attr("class", "axis axis--x")
+			.attr("transform", "translate(0," + height + ")")
+			.call(d3.axisBottom(x));
+	
+		g.append("g")
+			.attr("class", "axis axis--y")
+			.call(d3.axisLeft(y))
+			.append("text")
+			.attr("transform", "rotate(-90)")
+			.attr("y", 6)
+			.attr("dy", "0.71em")
+			.attr("text-anchor", "end")
+			.text("Frequency");
 }
 
 function onMouseOverCity_BarChart(event, cityData) {
@@ -27,31 +61,15 @@ function onMouseOverCity_BarChart(event, cityData) {
 		.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
 	const data = [
-		{ "Index": "COL", "Value": cityData["Cost of Living Index"] },
+		{ "Index": "Cost Living", "Value": cityData["Cost of Living Index"] },
 		{ "Index": "Groceries", "Value": cityData["Groceries Index"] },
 		{ "Index": "Restaurants", "Value": cityData["Restaurant Price Index"] },
 		{ "Index": "Rent", "Value": cityData["Rent Index"] },
 		{ "Index": "Purchasing Power", "Value": cityData["Local Purchasing Power Index"] }
 	]
 
-
 	x.domain(data.map(function (d) { return d.Index; }));
-	y.domain([0, 140]);
-
-	g.append("g")
-		.attr("class", "axis axis--x")
-		.attr("transform", "translate(0," + height + ")")
-		.call(d3.axisBottom(x));
-
-	g.append("g")
-		.attr("class", "axis axis--y")
-		.call(d3.axisLeft(y))
-		.append("text")
-		.attr("transform", "rotate(-90)")
-		.attr("y", 6)
-		.attr("dy", "0.71em")
-		.attr("text-anchor", "end")
-		.text("Frequency");
+	y.domain([0, 160]);
 
 	g.selectAll(".bar")
 		.data(data)
@@ -63,13 +81,15 @@ function onMouseOverCity_BarChart(event, cityData) {
 		.attr("height", function (d) { return height - y(parseInt(d.Value)); })
 		.attr("fill", function(d) {
 			switch(d.Index) {
-				case "COL": return "#400000";
-				case "Groceries": return"#7c0010";
-				case "Restaurants": return"#bd0011";
-				case "Rent": return "#ff1600";
-				case "Purchasing Power": return "#000080";
+				case "Cost Living": return "#e41a1c";
+				case "Groceries": return "#4daf4a";
+				case "Restaurants": return "#984ea3";
+				case "Rent": return "#377eb8";
+				case "Purchasing Power": return "#ff7f00";
 			}
 		});
+	
+		
 }
 
 
@@ -79,6 +99,9 @@ function onMouseOutOfCity_BarChart(event, cityData) {
 	console.log("Bar chart: mouse out of:");
 	console.log(cityData);
 
-	d3.select("#barchart").selectAll('g').remove();
+	let svg = d3.select("#barchart");
 
+	svg.selectAll(".bar")
+		.remove()
+		.transition();
 }
